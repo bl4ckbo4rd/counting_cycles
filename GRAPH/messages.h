d028f4d22347d9b57d8f3e816a0057165381f0c5
Enrico Ventura <ventura.1699775@studenti.uniroma1.it>
2021-04-12 10:57:59 +0200

    add struct BPiteration entropy L1T3

--- GRAPH/messages.h
+++ GRAPH/messages.h
@@ -12,6 +12,7 @@ struct L4 { };     //cycle of lenght 4
 
 struct L1T1 { };  //1 time basin of attraction of cycle of lenght 1
 struct L1T2 { };  //2 time basin of attraction of cycle of lenght 1
+struct L1T3 { };  //3 time basin of attraction of cycle of lenght 1
 
 class Messages{
 public:
@@ -44,6 +45,7 @@ public:
     
     inline void def_constraint(L1T1, int flag_red, int z, int n, vector <double>& vec_J_k_to_n);
     inline void def_constraint(L1T2, int flag_red, int z, int n, vector <double>& vec_J_k_to_n);
+    inline void def_constraint(L1T3, int flag_red, int z, int n, vector <double>& vec_J_k_to_n);
 
     
     template<typename Tag>
@@ -58,6 +60,7 @@ public:
     
     inline void def_constraint_bp(L1T1, int flag_red, int z, int l, vector <double>& vec_J_k_to_i, double J_j_to_i);
     inline void def_constraint_bp(L1T2, int flag_red, int z, int l, vector <double>& vec_J_k_to_i, double J_j_to_i);
+    inline void def_constraint_bp(L1T3, int flag_red, int z, int l, vector <double>& vec_J_k_to_i, double J_j_to_i);
     
     
     template<typename Tag>
@@ -82,6 +85,7 @@ public:
 
     void def_printEntropy(L1T1, double Part1, double Part2);
     void def_printEntropy(L1T2, double Part1, double Part2);
+    void def_printEntropy(L1T3, double Part1, double Part2);
 
     
     
@@ -97,6 +101,7 @@ public:
 
     void def_print_BPit(L1T1, double tmp_th, int t);
     void def_print_BPit(L1T2, double tmp_th, int t);
+    void def_print_BPit(L1T3, double tmp_th, int t);
     
     template<typename Tag>
     void look_up_table(int flag_red);                           //this function fills the vector allowed_conf for each node.
