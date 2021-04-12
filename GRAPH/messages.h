eb1157468d85ccc8ea4242ebb620a9c320592a89
Enrico Ventura <ventura.1699775@studenti.uniroma1.it>
2021-04-12 11:08:58 +0200

    add struct BPiteration entropy L2T2

--- GRAPH/messages.h
+++ GRAPH/messages.h
@@ -14,6 +14,7 @@ struct L1T1 { };  //1 time basin of attraction of cycle of lenght 1
 struct L1T2 { };  //2 time basin of attraction of cycle of lenght 1
 struct L1T3 { };  //3 time basin of attraction of cycle of lenght 1
 struct L2T1 { };  //1 time basin of attraction of cycle of lenght 2
+struct L2T2 { };  //2 time basin of attraction of cycle of lenght 2
 
 class Messages{
 public:
@@ -48,6 +49,7 @@ public:
     inline void def_constraint(L1T2, int flag_red, int z, int n, vector <double>& vec_J_k_to_n);
     inline void def_constraint(L1T3, int flag_red, int z, int n, vector <double>& vec_J_k_to_n);
     inline void def_constraint(L2T1, int flag_red, int z, int n, vector <double>& vec_J_k_to_n);
+    inline void def_constraint(L2T2, int flag_red, int z, int n, vector <double>& vec_J_k_to_n);
 
     
     template<typename Tag>
@@ -64,6 +66,7 @@ public:
     inline void def_constraint_bp(L1T2, int flag_red, int z, int l, vector <double>& vec_J_k_to_i, double J_j_to_i);
     inline void def_constraint_bp(L1T3, int flag_red, int z, int l, vector <double>& vec_J_k_to_i, double J_j_to_i);
     inline void def_constraint_bp(L2T1, int flag_red, int z, int l, vector <double>& vec_J_k_to_i, double J_j_to_i);
+    inline void def_constraint_bp(L2T2, int flag_red, int z, int l, vector <double>& vec_J_k_to_i, double J_j_to_i);
     
     
     template<typename Tag>
@@ -90,6 +93,7 @@ public:
     void def_printEntropy(L1T2, double Part1, double Part2);
     void def_printEntropy(L1T3, double Part1, double Part2);
     void def_printEntropy(L2T1, double Part1, double Part2);
+    void def_printEntropy(L2T2, double Part1, double Part2);
 
     
     
@@ -107,6 +111,7 @@ public:
     void def_print_BPit(L1T2, double tmp_th, int t);
     void def_print_BPit(L1T3, double tmp_th, int t);
     void def_print_BPit(L2T1, double tmp_th, int t);
+    void def_print_BPit(L2T2, double tmp_th, int t);
 
     
     template<typename Tag>
